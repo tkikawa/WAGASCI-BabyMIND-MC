6e8f173148fcc37af19e81cc91ef1090ece2666a
Tatsuya Kikawa <tatskikawa@gmail.com>
2018-11-01 14:41:27 +0900

    Modify the location of the magnetic field.

--- src/B2MagneticField.cc
+++ src/B2MagneticField.cc
@@ -31,17 +31,33 @@ void B2MagneticField::GetFieldValue(const G4double Point[3], G4double* Bfield) c
   Bfield[1]=0.*tesla;
   Bfield[2]=0.*tesla;
 
-  G4double x = Point[0]-45.*cm;
+  G4double x = Point[0]-60.*cm;
   G4double y = Point[1];	
   G4double z = Point[2]-(275.-200)*cm;
 
+  G4double iron_module_z[33] = {7, 14.2, 21,49.7, 60.7, 71.7,
+				95.9, 102.9, 113.7, 121, 145.2, 151.9,
+				163.1, 170, 193.8, 200.8, 207.8, 214.8,
+				243.7, 250.6, 257.8, 286.5, 293.7, 300.4,
+				307.4, 331.3, 338.3, 345.4, 352.1, 376,
+				383.3, 390.2, 397.1};
+
   if(x>Xmin&&x<Xmax&&y>Ymin&&y<Ymax&&z>Zmin&&z<Zmax){
+
 
     int xint = (int)(x-Xmin)/(Xmax-Xmin)*Xbin;
     int yint = (int)(y-Ymin)/(Ymax-Ymin)*Ybin;
-    Bfield[0] = Bx[xint][yint]*tesla;
-    Bfield[1] = By[xint][yint]*tesla;
-    Bfield[2] = Bz[xint][yint]*tesla;
+
+
+    for(int i=0; i<33; i++){
+      if(z>iron_module_z[i]*cm-3.0*cm/2&&z<iron_module_z[i]*cm+3.0*cm/2){
+	Bfield[0] = Bx[xint][yint]*tesla;
+	Bfield[1] = By[xint][yint]*tesla;
+	Bfield[2] = Bz[xint][yint]*tesla;
+	break;
+      }
+    }
+
   }
 
 }
