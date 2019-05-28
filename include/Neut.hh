a0b37f1125e543bdf49c894e53165ec1ba7abb63
Giorgio Pintaudi <giorgio.pimpa@gmail.com>
2019-05-28 09:44:09 +0900

    Tried to Fix the Neut.hh and Neut.cc files
    Still remains a nasty segmentation fault to fix

--- include/Neut.hh
+++ include/Neut.hh
@@ -1,13 +1,25 @@
 #ifndef Neut_h
 #define Neut_h 1
 
+// CERNLIB includes 
+#include "cfortran/cfortran.h"
+#include "cfortran/packlib.h"
+
 #define NVEC 100
 #define NG 20
-
-#include <stdio.h>
-#include <iostream>
-
 #define JNUBEAM10C
+#define NTNEUT 10
+
+/*
+  Note: the PAWC and all ntuple variables and structures MUST be here,
+  64 bit architectures require this to be loaded in the 32 bit address space.
+*/
+
+// define the PAWC common
+#define PAWC_SIZE 50000
+typedef struct { float PAW[PAWC_SIZE]; } PAWC_DEF;
+#define PAWC COMMON_BLOCK(PAWC, pawc)
+COMMON_BLOCK_DEF(PAWC_DEF, PAWC);
 
 //
 typedef struct {
@@ -18,8 +30,10 @@ typedef struct {
 
 //
 typedef struct {
-    int gipart;
-    float gpos0[3],gvec0[3],gamom0;
+  int gipart;
+  float gpos0[3];
+  float gvec0[3];
+  float gamom0;
 } PrimaryParticle;
 
 typedef struct {//for t2kreweight
@@ -48,115 +62,114 @@ typedef struct {//for t2kreweight
 
 //
 typedef struct {
-    int spid;
-    int pgen;
-    float psi0,xsi0[3],nsi0[3],cossi0bm;
-    float xsi[3];
-    int smech;
-    int intgt;
-    float prvtx[3];
-    int smed;
-    int gppid;
-    float xgpi0[3],xgpi[3],pgpi0;
-    int gpmech, gpmed;
-    int prmech, prmed;
-    int prdght, sdght, gpdght;
-    int chain;
+  int spid;
+  int pgen;
+  float psi0,xsi0[3],nsi0[3],cossi0bm;
+  float xsi[3];
+  int smech;
+  int intgt;
+  float prvtx[3];
+  int smed;
+  int gppid;
+  float xgpi0[3],xgpi[3],pgpi0;
+  int gpmech, gpmed;
+  int prmech, prmed;
+  int prdght, sdght, gpdght;
+  int chain;
 } OptionalVariables;
 
 //
 typedef struct {
-	int ng;
-	float gpx[NG],gpy[NG],gpz[NG],gcosbm[NG];
-	float gvx[NG],gvy[NG],gvz[NG];
-	int gpid[NG],gmec[NG];
+  int ng;
+  float gpx[NG],gpy[NG],gpz[NG],gcosbm[NG];
+  float gvx[NG],gvy[NG],gvz[NG];
+  int gpid[NG],gmec[NG];
 } Ancestor;
 
 //
 typedef struct {
-	float Enusk;
-	float normsk;
-	float anorm;
+  float Enusk;
+  float normsk;
+  float anorm;
 } SKInfo;
 
 //
 typedef struct {
-	int Numbndn;
-	int Numbndp;
-	int Numfrep;
-	int Numatom;
+  int Numbndn;
+  int Numbndp;
+  int Numfrep;
+  int Numatom;
 } TargetInfo;
 
 //
 typedef struct {
-	float Crsenergy;
-	float Totcrsne;		// total cross-section
-	float Difcrsne[8];
+  float Crsenergy;
+  float Totcrsne;		// total cross-section
+  float Difcrsne[8];
 } NeutCrs;
 
 //
-class Neut
-{
+typedef struct{
+  int EventNumber;
+  int Mode;
+  int NumParticle;
+  int ParticleID[NVEC];
+  float AbsMomentum[NVEC];
+  float Momentum[NVEC][3];
+} PrimaryVector;
+
+//
+typedef struct {
+  float AbsMomentum;
+  float Vertex[3];
+  float Direction[3];
+  float CosBeam;
+} ParentVector;
+
+//
+typedef struct{
+  float Energy;
+  int ParentID, ProductionMode;
+  ParentVector ParentDecay;
+  float Norm;
+  int nvtx0;
+  ParentVector ParentProduction;
+  float r, x, y, Direction[3];
+  int FDID;
+  PrimaryParticle primary_particle;
+  /*
+  //removed before using t2kreweight
+  #ifdef JNBEAM10A
+  OptionalVariables optional_variables;
+  #endif
+  #ifdef JNUBEAM10C
+  Ancestor ancestor;
+  SKInfo sk_info;
+  #endif
+  */
+} NeutNeutrino; 
+
+typedef struct {
   int NumberOfEvent;
+  int ID;
+  
+  PrimaryVector   Primary;
+  FsiHist         Fsi;//for t2kreweight
+  SecondaryVector Secondary;
+  NeutNeutrino Neutrino;
+	
+  NeutCrs neutcrs;//for t2kreweight
+  CrsInfo Crs;//for t2kreweight
 
-    typedef class ParentVector{
-      float AbsMomentum, Vertex[3], Direction[3], CosBeam;
-    } ;
+  TargetInfo target_info;//for t2kreweight
+  //NeutCrs neutcrs;//moved for t2kreweight
+} NeutVector;
 
-public:
-  struct{
+int NtupleReadInit(char*);
+int NtupleReadEvent(SecondaryVector&, float*);
 
-    int EventNumber;
-
-    struct{
-      int Mode;
-      int NumParticle, ParticleID[NVEC];
-      float AbsMomentum[NVEC], Momentum[NVEC][3];
-    } Primary;
-    
-
-    FsiHist         Fsi;//for t2kreweight
-    SecondaryVector Secondary;
-
-    NeutCrs neutcrs;//for t2kreweight
-    CrsInfo Crs;//for t2kreweight
-        
-    struct{
-      float Energy;
-      int ParentID, ProductionMode;
-      ParentVector ParentDecay;
-      float Norm;
-      int nvtx0;
-      ParentVector ParentProduction;
-      float r, x, y, Direction[3];
-      int FDID;
-      PrimaryParticle primary_particle;
-      /*
-//removed before using t2kreweight
-#ifdef JNBEAM10A
-      OptionalVariables optional_variables;
-#endif
-#ifdef JNUBEAM10C
-			Ancestor ancestor;
-      SKInfo sk_info;
-#endif
-      */
-    } Neutrino; 
-
-
-    TargetInfo target_info;//for t2kreweight
-    //NeutCrs neutcrs;//moved for t2kreweight
-
-
-  } Vector;
-
-	int ID;
-
-public:
-
-  int NtupleReadInit(char*);
-  int NtupleReadEvent(SecondaryVector&,float*);
-
-};
+extern NeutVector Vector;
+extern int zero;
+extern int ierr;
 
 #endif
